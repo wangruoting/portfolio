65caf38609dfb258b010c7016b5682eac5b70518
rwan0085 <rwan0085@student.monash.edu>
2022-11-11 13:11:12 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Project from './Project/Project.js';
+import About from './About/About.js';
+import Resume from './Resume/Resume.js';
+import Home from './Home/Home.js';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
