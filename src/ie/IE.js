59c68ff8e31848617de581181e3041e4df4e443a
rwan0085 <rwan0085@student.monash.edu>
2022-11-16 13:21:42 +1100

    project - ie page structure setup

--- src/ie/IE.js
+++ src/ie/IE.js
@@ -3,7 +3,27 @@ import './ie.css';
 export default function IE() {
     return (
         <>
-            <h1>Protect Children From Infections</h1>
+            <div className='whole-page-container'>
+                <div className='title'>
+                    <h1>Protect Children From Infections</h1>
+                    <h4 style={{ color: 'GrayText' }}>October 2022 - Participant in half of the website - Coder & Designer</h4>
+                </div>
+
+                <div className='ie-home-page-container'>
+                    <div style={{ width: '100%' }}>
+                        <img src='https://letsstoreinfolalal.s3.ap-southeast-2.amazonaws.com/ie-home+page+full.png' width='100%' height='auto'></img>
+                    </div>
+
+                    <div className='ie-description-section'>
+                        <h1>
+                            Graduate Project
+                        </h1>
+                        <h3>
+
+                        </h3>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
