bf1dd3e105f18e5d3db4c3ee185808399fcc8747
rwan0085 <rwan0085@student.monash.edu>
2023-07-04 12:37:19 +1000

    Missing pets project demo info uploaded

--- src/FIT5046/FIT5046.js
+++ src/FIT5046/FIT5046.js
@@ -11,11 +11,11 @@ export default function FIT5046() {
                 </div>
 
                 <div className='FIT5046-intro-container'>
-                    <div style={{ width: '100%' }}>
-                        <img className='FIT5046-landing-pic' src='https://letsstoreinfolalal.s3.ap-southeast-2.amazonaws.com/FIT5046_project_cover.png' ></img>
+                    <div >
+                        <img className='FIT5046-pic landing' src='https://letsstoreinfolalal.s3.ap-southeast-2.amazonaws.com/FIT5046_project_cover.png' ></img>
                     </div>
 
-                    <div className='FIT5046-description-section'>
+                    <div className='FIT5046-description-section intro'>
                         <h3>
                             An android app helps user to post & find their lost pets.
                         </h3>
@@ -24,18 +24,46 @@ export default function FIT5046() {
                             {/* Third-Party Login, Mapbox API, Weather API, Firebase Realtime Database, */}
                         </h5>
                     </div>
-
-                    {/* <div>
-                        <h1>hi</h1>
-                    </div> */}
                 </div>
+                <h2>Project Demo <i className="fa-solid fa-hand-sparkles"></i></h2>
+                <h5 style={{ color: 'GrayText', paddingTop: '10px' }}>Project purpose: Safely bring lost pets home.</h5>
+                <h5 style={{ color: 'GrayText' }}>Working in a multicultural agile team of 4.</h5>
 
+                <div className='FIT5046-intro-container'>
+                    <div>
+                        <img className='FIT5046-pic login' src='../5046-login.png'></img>
+                    </div>
 
-                {/* <div className='desc-below-container'>
-                    <h3>
-                        hi
-                    </h3>
-                </div> */}
+                    <div className='FIT5046-description-section login-desc'>
+                        <h4>User is required to login to secure their data and pets info.</h4>
+                        <h5 style={{ color: 'GrayText' }}>
+                            Implemented using FaceBook SDK & Firebase Database.
+                        </h5>
+                    </div>
+                    <div>
+                        <img className='FIT5046-pic home' src='../5046-home.png'></img>
+                    </div>
+                    <div className='FIT5046-description-section home-desc'>
+                        <h4>
+                            Home page showing post details of current missing or found pets.
+                        </h4>
+                        <h5 style={{ color: 'GrayText' }}>
+                            Implemented using Android room database, retrofit and Open Weather API.
+                        </h5>
+                    </div>
+
+                    <div>
+                        <img className='FIT5046-pic map' src='../5046-map-pet-location.png'></img>
+                    </div>
+                    <div className='FIT5046-description-section pet-detail-desc'>
+                        <h4>
+                            Each pet details pop up along with options that user prefer to choose.
+                        </h4>
+                        <h5 style={{ color: 'GrayText' }}>
+                            Implemented using Mapbox API, FaceBook SDK.
+                        </h5>
+                    </div>
+                </div>
             </div >
         </>
     );
